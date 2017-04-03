f9e617907dacc6cef1c334aa326838c43de600d4
calintat <calintat@gmail.com>
2017-04-03 17:13:13 +0100

    Finished update and added get methods

--- Assets/Scripts/CarAPI.cs
+++ Assets/Scripts/CarAPI.cs
@@ -1,38 +1,69 @@
-﻿
+﻿using System;
+
 public class CarAPI {
 
-  // possible actions
-  private int accel, turn;
+	// possible actions
+	private int accel, turn;
 
-  // accessible information – move to get methods?
-  public Point position, velocity;
-  public double turnAngle, tiltAngle, distanceToLeftSide, distanceToRightSide;
+	// accessible information
+	private Point position, velocity;
+	private double turnAngle, tiltAngle, distanceToLeftSide, distanceToRightSide;
 
-  internal void Update(GameState gameState) {
-    position.x = gameState.px;// save on needing to make new Point object
-    // TODO complete
-  }
+	internal void Update(GameState gameState) {
+		position.x = gameState.px; // save on needing to make new Point object
+		position.z = gameState.pz;
+		velocity.x = gameState.px;
+		velocity.z = gameState.pz;
+		turnAngle = Math.Min(gameState.turnAngle, gameState.maxSafeTurningAngle);
+		tiltAngle = gameState.tiltAngle;
+		distanceToLeftSide = gameState.getDistanceToLeftSide ();
+		distanceToRightSide = gameState.getDistanceToRightSide ();
+	}
 
-  internal CarAction getActionAndReset() {
-    CarAction r = new CarAction (accel, turn);
-    accel = turn = 0;
-    return r;
-  }
+	internal CarAction getActionAndReset() {
+		CarAction r = new CarAction (accel, turn);
+		accel = turn = 0;
+		return r;
+	}
 
-  // methods that the script calls
-  public void accelerate() {
-    accel = 1;
-  }
+	// methods that the script calls
+	public void accelerate() {
+		accel = 1;
+	}
 
-  public void decelerate() {
-    accel = -1;
-  }
+	public void decelerate() {
+		accel = -1;
+	}
 
-  public void turnRight() {
-    turn = 1;
-  }
+	public void turnRight() {
+		turn = 1;
+	}
 
-  public void turnLeft() {
-    turn = -1;
-  }
+	public void turnLeft() {
+		turn = -1;
+	}
+		
+	public Point getPosition() {
+		return position;
+	}
+
+	public Point getVelocity() {
+		return velocity;
+	}
+
+	public double getTurnAngle() {
+		return turnAngle;
+	}
+
+	public double getTiltAngle() {
+		return tiltAngle;
+	}
+
+	public double getDistanceToLeftSide() {
+		return distanceToLeftSide;
+	}
+
+	public double getDistanceToRightSide() {
+		return distanceToRightSide;
+	}
 }
