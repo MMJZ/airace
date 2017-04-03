fecf23cfb9c11f6995a194aa2640e5ce4a41ac45
calintat <calintat@gmail.com>
2017-04-03 17:12:12 +0100

    Added method to calculate distance to another point

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -1,9 +1,15 @@
-﻿public class Point {
+﻿using System;
 
-  public double x, z;
+public class Point {
 
-  public Point(double x, double z) {
-    this.x = x;
-    this.z = z;
-  }
+	public double x, z;
+
+	public Point(double x, double z) {
+		this.x = x;
+		this.z = z;
+	}
+
+	public double distanceTo(Point other) {
+		return Math.Sqrt(Math.Pow(this.x - other.x, 2.0) + Math.Pow(this.z - other.z, 2.0));
+	}
 }
