a906bbfccf200bad5396791cc4ed005880cb6c0d
Daniel Mroz <daniel.mroz@univ.ox.ac.uk>
2017-04-13 15:52:45 +0100

    filled out RaceEnv

--- Assets/Scripts/RaceEnvironment.cs
+++ Assets/Scripts/RaceEnvironment.cs
@@ -3,66 +3,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.CompilerServices;
 
-public class RaceEnvironment : MonoBehaviour {
+namespace UnityStandardAssets.Vehicles.Car {
+  public class RaceEnvironment : MonoBehaviour {
 
-  private Track[] tracks = new Track[] {
-    new Track (new Node (0, 0, 0, 0), new Node (0, 1, 0, 0)),
-    new Track (new Node (0, 0, 0, 0), new Node (0, 1, 0, 0))
-  };
+    [SerializeField] private GameObject car;
 
-  private int nocars = 1;
-  private Simulator[] simulators;
-  private Track track;
-  private int timer;
 
-  void Awake() {
+    private class Racer{
+      private Simulator simulator;
+      private GameState state;
+      private GameObject car;
+      private CarController controller;
 
-    // TODO get this info from main menu script object
-    string script = null;
-    string nickname = null;
-    int trackno = 0;
-    int racetype = 0;
-    Leaderboard leaderboard = null;
+      public Racer(GameObject c, string script, GameState t){
+        car = c;
+        controller = car.GetComponent<CarController> ();
+        state = t;
+        try {
+          simulator = new Simulator();
+          simulator.loadScript (script);
+          simulator.start(t);
+        } catch (NLua.Exceptions.LuaException e) {
+          return; // show error
+        }
+      }
+      public void update(){
+        try {
+          CarAction action = simulator.update (state);
+          controller.Move (action.turn, action.accel, action.accel, 0);
+          state.setTurnAngle (controller.CurrentSteerAngle);
+          //TODO: Should change these to Vector3 in GameState
+          state.setPositionX (car.transform.position.x);
+          state.setPositionZ (car.transform.position.z);
+          state.setVelocityX (controller.CurrentVelocity.x);
+          state.setVelocityZ (controller.CurrentVelocity.z);
+        } catch (NLua.Exceptions.LuaException e) {
+          return; // show error
+        }
+      }
 
-    if(racetype == 0) {
-      nocars = 1;
     }
 
-    simulators = new Simulator[nocars];
-    for (int x = 0; x < nocars; x++)
-      try {
-        simulators [x] = new Simulator ();
-        simulators [x].loadScript (script);
-      } catch (NLua.Exceptions.LuaException e) {
-        return; // show error
+    //TODO: Maybe turn this into an array, so we can have multiple cars at the same time?
+    private Racer racer;
+
+    //TODO: Make some tracks
+    private Track[] tracks = new Track[] {
+      new Track (new Node (0, 0, 0, 0), new Node (0, 100, 0, 0), new Node (0, 200, 0, 0), new Node (0, 300, 0, 0), new Node (0, 400, 0, 0), new Node (0, 500, 0, 0)),
+      new Track (new Node (0, 0, 0, 0), new Node (0, 1, 0, 0))
+    };
+
+    private int numCars = 1;
+    private Track track;
+    private int timer;
+
+    void Awake() {
+      // TODO get this info from main menu script object
+      string script = "";
+      string nickname = "";
+      int trackNum = 0;
+      int racetype = 0;
+      timer = 0;
+      //TODO: Leaderboard is bugged currently, uncomment when fixed
+      //Leaderboard leaderboard = new Leaderboard();
+
+      //TODO: figure out what to do here
+      if(racetype == 0) {
+        numCars = 1;
       }
-    track = tracks [trackno];
-    // build objects to represent track
-    // build car object(s)
-  }
 
-  void Start() {
-    timer = 0;
-    // generate first game state
-    GameState state = null;
-    foreach (Simulator s in simulators)
-      s.start (state);
-  }
+      car = GameObject.FindGameObjectWithTag ("Car");
+      track = tracks [trackNum];
+      // TODO: build objects to represent track
+     
 
-  void FixedUpdate() {
-    timer += 1;
-    GameState state = null;
-    // generate game state from car
-    // update timer display
-    bool gameover = false;
-    if(gameover) {
-      // flash end
-      // stop update from being called
-      // change scene to end menu
-    } else {
-      foreach (Simulator s in simulators) {
-        CarAction action = s.update (state);
-        // apply action to car object
+
+      GameState state = new GameState(track.nodes[track.nodes.Length-1], track.nodes[0], track.nodes[1],timer);
+      racer = new Racer (car, script, state);
+
+    }
+
+    void FixedUpdate() {
+      timer += 1;
+      // update timer display
+      bool gameover = false;
+      if(gameover) {
+        // TODO: flash end
+        // TODO: stop update from being called
+        // TODO: change scene to end menu
+      } 
+      else {          
+        racer.update ();
       }
     }
   }
