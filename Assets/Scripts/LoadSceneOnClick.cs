ac9c18bb7534d818d1f6b40df8da0c8b6e6df897
PaulHorvathBojan <paul.horvath.bojan@gmail.com>
2017-04-14 01:09:02 +0300

    Added Main Menu
    in this particular commit, scripts for quitting and making the transition to another scene

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//basically what script does is define a function to be used to load another scene
+//found some 1-hour tutorial on the internet about building menus in Unity
+//used same variable/ class names for ease of understanding
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex) {
+			
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
---
+++ Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour {
+
+	public void Quit() {
+
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+		//what above 5 lines do: if using the editor, exit play mode, else exit app
+	}
+}
